b39fc473c15e81ce66ab50e317bd938795e1870d
test1 user <test1@example.com>
2026-04-20 15:23:30 +0800

    Import MDK-ARM project into root test1 repository and add ignore rules for build artifacts

---
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -0,0 +1,160 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file         stm32f4xx_hal_msp.c
+  * @brief        This file provides code for the MSP Initialization
+  *               and de-Initialization codes.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN Define */
+
+#define KEY0_PIN GPIO_PIN_0
+#define KEY0_PORT GPIOA
+#define KEY1_PIN GPIO_PIN_13
+#define KEY1_PORT GPIOC
+#define KEY2_PIN GPIO_PIN_2
+#define KEY2_PORT GPIOH
+#define WK_UP_PIN GPIO_PIN_3
+#define WK_UP_PORT GPIOH
+
+#define KEY0_PRES 1
+#define KEY1_PRES 2
+#define KEY2_PRES 3
+#define WKUP_PRES 4
+
+/* USER CODE END Define */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN Macro */
+
+/* USER CODE END Macro */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+static uint8_t key_up = 1;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+void KEY_Init(void);
+uint8_t KEY_Scan(uint8_t mode);
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+
+/* USER CODE END PFP */
+
+/* External functions --------------------------------------------------------*/
+/* USER CODE BEGIN ExternalFunctions */
+
+/* USER CODE END ExternalFunctions */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+/**
+  * Initializes the Global MSP.
+  */
+void HAL_MspInit(void)
+{
+
+  /* USER CODE BEGIN MspInit 0 */
+
+  /* USER CODE END MspInit 0 */
+
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* System interrupt init*/
+
+  /* USER CODE BEGIN MspInit 1 */
+
+  /* USER CODE END MspInit 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+void KEY_Init(void)
+{
+    GPIO_InitTypeDef GPIO_Initure;
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+
+    GPIO_Initure.Pin = KEY0_PIN;
+    GPIO_Initure.Mode = GPIO_MODE_INPUT;
+    GPIO_Initure.Pull = GPIO_PULLDOWN;
+    GPIO_Initure.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(KEY0_PORT, &GPIO_Initure);
+
+    GPIO_Initure.Pin = KEY1_PIN;
+    GPIO_Initure.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(KEY1_PORT, &GPIO_Initure);
+
+    GPIO_Initure.Pin = KEY2_PIN | WK_UP_PIN;
+    HAL_GPIO_Init(KEY2_PORT, &GPIO_Initure);
+}
+
+uint8_t KEY_Scan(uint8_t mode)
+{
+    if (mode == 1) key_up = 1;
+    if (key_up && (HAL_GPIO_ReadPin(KEY0_PORT, KEY0_PIN) == GPIO_PIN_RESET ||
+                   HAL_GPIO_ReadPin(KEY1_PORT, KEY1_PIN) == GPIO_PIN_RESET ||
+                   HAL_GPIO_ReadPin(KEY2_PORT, KEY2_PIN) == GPIO_PIN_RESET ||
+                   HAL_GPIO_ReadPin(WK_UP_PORT, WK_UP_PIN) == GPIO_PIN_SET))
+    {
+        HAL_Delay(10);
+        key_up = 0;
+        if (HAL_GPIO_ReadPin(KEY0_PORT, KEY0_PIN) == GPIO_PIN_RESET)
+            return KEY0_PRES;
+        else if (HAL_GPIO_ReadPin(KEY1_PORT, KEY1_PIN) == GPIO_PIN_RESET)
+            return KEY1_PRES;
+        else if (HAL_GPIO_ReadPin(KEY2_PORT, KEY2_PIN) == GPIO_PIN_RESET)
+            return KEY2_PRES;
+        else if (HAL_GPIO_ReadPin(WK_UP_PORT, WK_UP_PIN) == GPIO_PIN_SET)
+            return WKUP_PRES;
+    }
+    else if (HAL_GPIO_ReadPin(KEY0_PORT, KEY0_PIN) == GPIO_PIN_SET &&
+             HAL_GPIO_ReadPin(KEY1_PORT, KEY1_PIN) == GPIO_PIN_SET &&
+             HAL_GPIO_ReadPin(KEY2_PORT, KEY2_PIN) == GPIO_PIN_SET &&
+             HAL_GPIO_ReadPin(WK_UP_PORT, WK_UP_PIN) == GPIO_PIN_RESET)
+    {
+        key_up = 1;
+    }
+    return 0;
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    if(GPIO_Pin == GPIO_PIN_13)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    }
+}
+
+/* USER CODE END 1 */
